127b5e997d3341c853ee3a8247ef7c1412c541d8
Syed Husamuddin <syedhusamuddin2@gmail.com>
2026-07-19 23:51:07 +0530

    git commit -m "feat: complete curated sports knowledge base for RAG ingestion"

---
+++ src/constants/sports.py
@@ -0,0 +1,22 @@
+SPORTS = [
+    "Cricket",
+    "Football",
+    "Basketball",
+    "Tennis",
+    "Badminton",
+    "Baseball",
+    "Olympics",
+]
+
+DIFFICULTIES = [
+    "Easy",
+    "Medium",
+    "Hard",
+]
+
+SOURCE = {
+    "type": "curated",
+    "name": "Sports Knowledge Base v1",
+}
+
+DATASET_VERSION = "1.0"
